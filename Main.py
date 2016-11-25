9cc3e8272ec707040698d7e344c8caeb302b11b8
Chen Cheng <1143556730@qq.com>
2016-11-25 11:18:19 +0800

    完善

--- Main.py
+++ Main.py
@@ -12,44 +12,52 @@ app = QtWidgets.QApplication(sys.argv)
 widget = QtWidgets.QWidget()
 widget1 = QtWidgets.QMainWindow()
 widget2 = QtWidgets.QDialog()
-control = requests.get("http://www.parallelc.cn/list.html")
 version = '1.2'
 
 
 def upgrade():
-    new = re.findall('version:(.*)', control.text)
-    return new[0] != version
+    try:
+        control = requests.get("http://www.parallelc.cn/list.html")
+    except Exception:
+        return False
+    else:
+        new = re.findall('version:(.*)', control.text)
+        if new:
+            return new[0] != version
+        else:
+            return False
 
 
 def try_go():
     try:
         html = Handle.login(ui.user_name.text(), ui.password.text(), ui.yzm.text())
-    except requests.exceptions.ConnectionError:
-        ui.warn.setText("连接失败!请检查网络")
-    except Exception as e:
-        ui.warn.setText("发生错误!" + str(e))
-    else:
         error = re.findall('<strong><font color="#990000">(.*?)</font></strong><br>', html.text)
         if error:
             ui.warn.setText(error[0])
             ui.img.show_img()
-        else:
-            go_list = re.findall("#(.*)", control.text)
-            right = 0
-            for i in go_list:
-                if re.match(i, ui.user_name.text()):
-                    right = 1
-                    break
-            if right == 0:
-                ui.warn.setText("你没有使用权限!")
-            else:
-                widget.close()
-                ui1.clear()
-                ui1.progressBar.setValue(0)
-                info = Handle.get_info()
-                ui1.number.setText(info[0])
-                ui1.user_name.setText(info[1])
-                widget1.show()
+            return
+        control = requests.get("http://www.parallelc.cn/list.html")
+        go_list = re.findall("#(.*)", control.text)
+        right = 0
+        for i in go_list:
+            if re.match(i, ui.user_name.text()):
+                right = 1
+                break
+        if right == 0:
+            raise Exception("你没有使用权限!")
+    except requests.exceptions.ConnectionError:
+        ui.warn.setText("连接失败!请检查网络")
+    except Exception as e:
+        ui.img.show_img()
+        ui.warn.setText("发生错误!" + str(e))
+    else:
+        widget.close()
+        ui1.clear()
+        ui1.progressBar.setValue(0)
+        info = Handle.get_info()
+        ui1.number.setText(info[0])
+        ui1.user_name.setText(info[1])
+        widget1.show()
 
 
 def logout():
@@ -71,7 +79,7 @@ if __name__ == "__main__":
                                                QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
                                                QtWidgets.QMessageBox.Yes)
         if reply == QtWidgets.QMessageBox.Yes:
-            webbrowser.open("http://www.parallelc.cn")
+            webbrowser.open("http://www.parallelc.cn/pj.zip")
 
     ui.login.clicked.connect(try_go)
     ui1.logout.triggered.connect(logout)
